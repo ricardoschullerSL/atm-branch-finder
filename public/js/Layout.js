f40c2b82810f7e853996eb34e028e30fd0d63b39
ricardoschullerSL <rschuller@scottlogic.com>
2017-07-18 10:53:16 +0100

    Removed silly line from the top header.

--- public/js/Layout.js
+++ public/js/Layout.js
@@ -22,8 +22,8 @@ export default class Layout extends React.Component {
             <h1><center>ATM Branch Finder</center></h1>
             <p>Work in progress</p>
             <p>Find any ATM or branch nearby. You can also look for a new Personal Current Account!</p>
-            <p>Click this super secret button to get your account info!</p>
-            {/* <a href='/account_info'><button onClick={this.authenticate}>Get Account Info</button></a> */}
+            {/*<p>Click this super secret button to get your account info!</p>
+             <a href='/account_info'><button onClick={this.authenticate}>Get Account Info</button></a> */}
         </div>
             <div className="header">
                 <BankContainer />
